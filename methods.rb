9c1b81907b1aac78346644f62ad38ecdfe280278
dancun616 <dancun616@gmail.com>
2023-02-07 16:44:46 +0300

    complete

--- methods.rb
+++ methods.rb
@@ -1,1 +1,48 @@
 # Your code here!
+
+def greet_programmer 
+
+  puts "Hello, programmer!"
+
+end
+
+greet_programmer
+
+
+def greet(name = "Sanchez") 
+
+  puts "Hello, #{name}!"
+
+end
+greet 
+
+def greet_with_default (name = "Sanchez") 
+
+  puts "Hello, #{name}!"
+
+  puts "Hello, programmer!"
+
+  end
+greet_with_default 
+
+def add a,b 
+
+ a + b
+
+end
+
+add 2, 5
+
+def halve x 
+
+  if x.class != Integer
+
+  return nil
+
+  end
+
+ return x/2
+
+end
+
+halve 6
